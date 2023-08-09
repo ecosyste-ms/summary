19492ec23bbd92d4dce72dc8f0d23613ad683165
Andrew Nesbitt <andrewnez@gmail.com>
2023-08-09 16:45:04 +0100

    sync a project if it is looked up and hasn't been recently synced

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -14,10 +14,12 @@ class Api::V1::ProjectsController < Api::V1::ApplicationController
       @project = Project.create(url: params[:url])
       @project.sync_async
     end
+    @project.sync_async if @project.last_synced_at.nil? || @project.last_synced_at < 1.day.ago
   end
 
   def ping
     @project = Project.find(params[:id])
     @project.sync_async
+    render json: { message: 'pong' }
   end
 end
